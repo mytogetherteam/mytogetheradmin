dadfc9d535531609a97bde9006e728b12823c6f5
Marco.Htet <marco.htet@ravuslaw.com>
2026-06-10 09:12:57 +0630

    fix type error

--- src/pages/categories/ManageCategories.tsx
+++ src/pages/categories/ManageCategories.tsx
@@ -7,7 +7,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import ConfirmDialog from "@/components/common/ConfirmDialog";
